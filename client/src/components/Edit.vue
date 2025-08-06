a86543e458632dc588b15c7be256223e8786a0bf
Espereon <announcement32@post.com>
2025-08-06 16:39:48 +0300

    add style

--- client/src/components/Edit.vue
+++ client/src/components/Edit.vue
@@ -2,25 +2,27 @@
 import { ref } from "vue";
 import axios from "axios";
 
+const formData = ref<FormData>({
+  inn: "",
+  street: "",
+  name: "",
+  brand: "",
+  locnumber: "",
+});
+const responseMessage = ref("");
+
 interface FormData {
   inn: string;
   street: string;
   name: string;
   brand: string;
+  locnumber: string;
 }
 
 const validateFieldLength = (field: string, maxLength: number) =>
   field.length <= maxLength;
 
 const validateINN = (inn: string) => /^\d+$/.test(inn);
-
-const formData = ref<FormData>({
-  inn: "",
-  street: "",
-  name: "",
-  brand: "",
-});
-const responseMessage = ref("");
 
 async function submitData() {
   try {
@@ -44,45 +46,128 @@ async function submitData() {
 </script>
 
 <template>
-  <div>
-    <form @submit.prevent="submitData">
-      <label for="inn">ИНН:</label><br />
-      <input
-        id="inn"
-        v-model="formData.inn"
-        type="text"
-        pattern="\d+"
-        maxlength="24"
-      /><br />
-      <label for="street">Улица:</label><br />
-      <input
-        id="street"
-        v-model="formData.street"
-        type="text"
-        maxlength="24"
-      /><br />
-      <label for="name">Имя:</label><br />
-      <input
-        id="name"
-        v-model="formData.name"
-        type="text"
-        maxlength="24"
-      /><br />
-      <label for="brand">Брэнд:</label><br />
-      <input
-        id="brand"
-        v-model="formData.brand"
-        type="text"
-        maxlength="24"
-      /><br />
-      <button type="submit">Отправить</button>
-    </form>
-    <p>{{ responseMessage }}</p>
+  <div class="flex items-center gap-60">
+    <div class="center pt-10">
+      <form class="grid text-field" @submit.prevent="submitData">
+        <label class="text-field__label" for="locnumber"
+          >Локальный номер<input
+            v-model="formData.locnumber"
+            class="text-field__input"
+            type="text"
+        /></label>
+        <label class="text-field__label" for="inn">ИНН:</label><br />
+        <input
+          class="text-field__input"
+          id="inn"
+          v-model="formData.inn"
+          type="text"
+          pattern="\d+"
+          maxlength="24"
+        /><br />
+        <label class="text-field__label" for="street">Улица:</label><br />
+        <input
+          class="text-field__input"
+          id="street"
+          v-model="formData.street"
+          type="text"
+          maxlength="24"
+        /><br />
+        <label class="text-field__label" for="name">Имя:</label><br />
+        <input
+          class="text-field__input"
+          id="name"
+          v-model="formData.name"
+          type="text"
+          maxlength="24"
+        /><br />
+        <label class="text-field__label" for="brand">Брэнд:</label><br />
+        <input
+          class="text-field__input"
+          id="brand"
+          v-model="formData.brand"
+          type="text"
+          maxlength="24"
+        /><br />
+        <button type="submit">Отправить</button>
+      </form>
+
+      <p>{{ responseMessage }}</p>
+    </div>
+    <div class="container flex flex-col gap-2">
+      <div>ОТЧЕТ ЗА СМЕНУ</div>
+      <div>{{ formData.name }}</div>
+      <div>ИНН {{ formData.inn }}</div>
+      <div>{{ formData.street }}</div>
+      <div>{{ formData.brand }}</div>
+      <div>НАЧАЛО 30/03/03 9:00:01</div>
+      <div>ОКОНЧА 30/03/03 17:00:59</div>
+      <div class="flex items-center justify-between">
+        <div>Терминал</div>
+        <div>№ {{ formData.locnumber.slice(4, 8) }}</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+
+.text-field {
+  margin-bottom: 1rem;
+}
+.text-field__label {
+  display: block;
+  margin-bottom: 0.25rem;
+}
+.text-field__input {
+  display: block;
+  width: 500px;
+  height: calc(2.25rem + 2px);
+  padding: 0.375rem 0.75rem;
+  font-family: inherit;
+  font-size: 1rem;
+  font-weight: 400;
+  line-height: 1.5;
+  color: #212529;
+  background-color: #fff;
+  background-clip: padding-box;
+  border: 1px solid #bdbdbd;
+  border-radius: 0.25rem;
+  transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+}
+
+.btn-new {
+  width: 200px;
+  height: 50px;
+  border-radius: 10px;
+  color: white;
+  transition: 0.2s linear;
+  background: #0b63f6;
+}
+
+.btn-new:hover {
+  box-shadow: 0 0 0 2px white, 0 0 0 4px #3c82f8;
+}
+
+.container {
+  font-family: "Courier New", Courier, monospace;
+  font-size: 12px;
+  line-height: 1.5;
+  height: 250px;
+  width: 230px;
+  margin: 10px auto;
+  padding: 15px;
+  background-color: #fff;
+  border: 1px solid #ccc;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+  text-align: center;
+}
+
+.container div {
+  height: 20px;
 }
 </style>
--- server/server.js
+++ server/server.js
@@ -1,4 +1,3 @@
-// Импортируем библиотеки
 const express = require("express");
 const cors = require("cors");
 const fs = require("fs/promises");
@@ -6,75 +5,68 @@ const DOMParser = require("@xmldom/xmldom").DOMParser;
 const xpath = require("xpath");
 const iconv = require("iconv-lite");
 
-// Создаем экземпляр приложения
 const app = express();
 const PORT = 3001;
 
-// Разрешаем CORS для всех доменов (для разработки)
 app.use(cors());
-// Позволяем Express обрабатывать JSON в теле запроса
 app.use(express.json());
 
-// Маршрут для приема данных
 app.post("/api/data", async (req, res) => {
   try {
-    // Извлекаем данные напрямую из тела запроса
-    const { inn, street, name, brand } = req.body;
-
-    // Читаем оригинальный XML-файл
+    const { inn, street, name, brand, locnubmer } = req.body;
     const buffer = await fs.readFile("./src/20050001.xml");
     const originalXml = iconv.decode(buffer, "windows-1251");
-
-    // Парсим XML-документ
     const doc = new DOMParser().parseFromString(originalXml, "application/xml");
 
-    // Обновляем значение ИНН
     const paramInn = xpath.select1("//parameter[@ID='105']", doc);
     if (paramInn) {
-      const valueNode = paramInn.getElementsByTagName("value")[0]; // Выбираем элемент <value>
+      const valueNode = paramInn.getElementsByTagName("value")[0];
       if (valueNode) {
-        valueNode.textContent = inn.trim(); // Устанавливаем новое значение в <value>
+        valueNode.textContent = inn.trim();
       }
     }
 
-    // Обновляем улицу
     const paramStreet = xpath.select1("//parameter[@ID='106']", doc);
     if (paramStreet) {
-      const valueNode = paramStreet.getElementsByTagName("value")[0]; // Выбираем элемент <value>
+      const valueNode = paramStreet.getElementsByTagName("value")[0];
       if (valueNode) {
-        valueNode.textContent = street.trim(); // Устанавливаем новое значение в <value>
+        valueNode.textContent = street.trim();
       }
     }
 
-    // Обновляем имя
     const paramName = xpath.select1("//parameter[@ID='31340']", doc);
     if (paramName) {
-      const valueNode = paramName.getElementsByTagName("value")[0]; // Выбираем элемент <value>
+      const valueNode = paramName.getElementsByTagName("value")[0];
       if (valueNode) {
-        valueNode.textContent = name.trim(); // Устанавливаем новое значение в <value>
+        let spaceCount = Math.floor((24 - name.length) / 2);
+        console.log(spaceCount);
+
+        if (name.length >= 23) {
+          valueNode.textContent = `${name.trim()}`;
+        } else {
+          let spacesBefore = Array(spaceCount + 1)
+            .fill(" ")
+            .join("");
+          valueNode.textContent = spacesBefore + name.trim();
+          console.log("условие соблюдено");
+        }
       }
     }
 
-    // Обновляем бренд
     const paramBrand = xpath.select1("//parameter[@ID='31341']", doc);
     if (paramBrand) {
-      const valueNode = paramBrand.getElementsByTagName("value")[0]; // Выбираем элемент <value>
+      const valueNode = paramBrand.getElementsByTagName("value")[0];
       if (valueNode) {
-        valueNode.textContent = brand.trim(); // Устанавливаем новое значение в <value>
+        valueNode.textContent = brand.trim();
       }
     }
 
-    // Конвертируем обновленный XML в строку
     const updatedXml = doc.toString();
 
-    // Кодируем в исходную кодировку
     const encodedXml = iconv.encode(updatedXml, "windows-1251");
 
-    // Сохраняем изменённый XML в новую папку
-    await fs.mkdir("./output", { recursive: true });
-    await fs.writeFile("./output/new_sample.xml", encodedXml);
+    await fs.writeFile(`./output/${locnubmer}.xml`, encodedXml);
 
-    // Ответ пользователю
     res
       .status(200)
       .json({ message: "Данные успешно получены!", data: req.body });
@@ -84,7 +76,6 @@ app.post("/api/data", async (req, res) => {
   }
 });
 
-// Запуск сервера
 app.listen(PORT, () => {
   console.log(`Сервер работает на http://localhost:${PORT}`);
 });
